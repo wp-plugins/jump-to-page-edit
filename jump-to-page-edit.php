90406234d6b70ed1921cbddf89dc3123dce7f63b
linkedbinary900 <linkedbinary900@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-28 23:02:11 +0000

    11/28/2012 - Version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/jump-to-page-edit/trunk@631502 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jump-to-page-edit.php
+++ jump-to-page-edit.php
@@ -3,7 +3,7 @@
 Plugin Name: Jump to - Page Edit
 Plugin URI: http://www.runwalkweb.com/wp/?page_id=28
 Description: Adds an option to the "Edit Page" screen, allowing the user to easily choose another page to edit without leaving the "Edit Page" screen.
-Version: 1.0
+Version: 1.0.1
 Author: zach_rww
 Author URI: http://www.runwalkweb.com
 License: GPL2
@@ -27,44 +27,66 @@ License: GPL2
 
 add_action('admin_menu', 'jump_to_page_edit_box');
 
-function jump_to_page_edit_box() {
-add_meta_box('post_info', 'Jump to - Page Edit', 'jump_to_page_edit_info', 'page', 'side', 'high');
-}
+	function jump_to_page_edit_box() {
+		add_meta_box('post_info', 'Jump to - Page Edit', 'jump_to_page_edit_info', 'page', 'side', 'high');
+	}
 
-//Adds the actual option box
-function jump_to_page_edit_info() {
-global $page;
-?>
+	//Adds the actual option box
+	function jump_to_page_edit_info() {
+		global $page;
+		?>
 <fieldset id="jump_to_page_edit-div">
 <div>
 <p>
 
 <label for="jump_to_page_edit">Choose Page:</label><br />
-<select name="jump_to_page_edit"
- onchange='document.location.href=this.options[this.selectedIndex].value;'> 
- <option value="">
-<?php echo esc_attr( __( 'Select page' ) ); ?></option> 
- <?php
-  $pages = get_pages( array( 'post_status' => 'publish,private,draft' ) ); 
-  $blog_home = home_url();
-  foreach ( $pages as $page ) {
-	$page_title_value = substr($page->post_title,0,26).'...';
-  	$option = '<option value="' . $blog_home . '/wp-admin/post.php?post=' . $page->ID . '&action=edit">';
-if ( $page->post_status == 'publish' ) {
-	$option .= '&#10003; ';
-}
-else {
-	$option .= '[' . $page->post_status . '] ';
-}
-	$option .= $page_title_value;
-	$option .= '</option>';
-	echo $option;
-  }
- ?>
+<select name="jump_to_page_edit" onchange='document.location.href=this.options[this.selectedIndex].value;'> 
+<option<?php selected( get_post_meta($page->ID, 'jump_to_page_edit', true), 'none' ); ?>><?php echo esc_attr( __( 'Select page' ) ); ?></option>
+<?php
+$pages = get_pages( array( 'post_status' => 'publish,private,draft' ) ); 
+$blog_home = home_url();
+	foreach ( $pages as $page ) {
+		$page_title_value = substr($page->post_title,0,26).'...';
+		$option = '<option value="' . $blog_home . '/wp-admin/post.php?post=' . $page->ID . '&action=edit">';
+			if ( $page->post_status == 'publish' ) {
+				$option .= '&#10003; ';
+			}
+			else {
+				$option .= '[' . $page->post_status . '] ';
+			}
+		$option .= $page_title_value;
+		$option .= '</option>';
+		echo $option;
+	}
+?>
 </select><br />
 
 </p>
 </div>
 </fieldset>
-<?php
+		<?php }
+
+add_action('save_page', 'jtpagei_save');
+	$pageID = $page->ID;
+
+	function jtpagei_save($pageID){
+	// called after a post or page is saved
+		if($parent_id = wp_is_post_revision($pageID)) {
+			$pageID = $parent_id;
+		}
+
+		if ($_POST['jump_to_page_edit']) {
+			update_jtpagei_meta($pageID, $_POST['jump_to_page_edit'], 'jump_to_page_edit');
+		}
+}
+
+	function update_jtpagei_meta($pageID, $newvalue, $field_name) {
+	// To create new meta
+		if(!get_post_meta($pageID, $field_name)){
+			add_post_meta($pageID, $field_name, $newvalue);
+		}
+		else{
+			// or to update existing meta
+			update_post_meta($pageID, $field_name, $newvalue);
+		}
 }
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link:
 Tags: page, edit, admin
 Requires at least: 3.3
 Tested up to: 3.4.2
-Stable tag: 1.0
+Stable tag: 1.0.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -35,6 +35,12 @@ No FAQ with this release... so far.
 
 
 == Changelog ==
+
+11/28/2012:  Version 1.0.1 - Updated plugin to fix problems occurring upon Pages being saved.
+			~ Custom Meta info was being altered.
+			~ My apologies.
+			~ "Jump to - Page Edit" will use a Custom Meta Field, jump_to_page_edit.
+			~ Because the selection of an option from the "Jump to - Page Edit" list does not actually require data to be saved to the current Page, the value of the Custom Meta Key 'jump_to_page_edit' does not need to be altered.
 
 11/24/2012:  Version 1.0 submission to WordPress.org
 
